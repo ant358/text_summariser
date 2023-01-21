3a8fda67791e09f1b034b30520ea189e150da6ae
Anthony Wynne <a.wynne@svgc.co.uk>
2023-01-21 20:29:40 +0000

    major structural changes

--- src/get_models.py
+++ src/get_models.py
@@ -1,11 +1,10 @@
 # %%
 import torch
+import logging
 from transformers import (
-                          T5Tokenizer,
-                          T5ForConditionalGeneration,
-                          AutoTokenizer,
-                          AutoModelForTokenClassification
-                          )
+    T5Tokenizer,
+    T5ForConditionalGeneration,
+)
 
 
 def get_t5_model():
@@ -18,17 +17,6 @@ def get_t5_tokenizer():
     return T5Tokenizer.from_pretrained('t5-large')
 
 
-def get_ner_tokenizer():
-    # load the tokenizer
-    return AutoTokenizer.from_pretrained('dslim/bert-base-NER')
-
-
-def get_ner_model():
-    # load the model
-    return AutoModelForTokenClassification.from_pretrained(
-                                         'dslim/bert-base-NER')
-
-
 def save_model(model, tokenizer, path):
     model.save_pretrained(path)
     tokenizer.save_pretrained(path)
@@ -38,12 +26,8 @@ def get_and_save_all_models():
     # download the models
     get_t5_model()
     get_t5_tokenizer()
-    get_ner_model()
-    get_ner_tokenizer()
-    save_model(model, tokenizer, './models/t5-large')
+    save_model(model, tokenizer, '../models/t5-large')
     print('t5_model saved')
-    save_model(model, tokenizer, './models/dslim/bert-base-NER')
-    print('ner_model saved')
 
 
 if __name__ == "__main__":
@@ -54,11 +38,5 @@ if __name__ == "__main__":
     tokenizer = get_t5_tokenizer()
     save_model(model, tokenizer, './models/t5-large')
     print('t5_model saved')
-
-    model = get_ner_model()
-    tokenizer = get_ner_tokenizer()
-    save_model(model, tokenizer, './models/dslim/bert-base-NER')
-    print('ner_model saved')
-
 
 # %%
